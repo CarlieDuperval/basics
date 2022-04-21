15e11e9ee15b2887259d04b1e104559456d9a95a
Carlie Duperval <carlierichell@gmail.com>
2022-04-21 17:13:07 -0400

    added fetch : arrays of beers and coffee

--- fetch.js
+++ fetch.js
@@ -40,12 +40,63 @@ import fetch from 'node-fetch'
 
 
 
-fetch("https://api.sampleapis.com/beers/ale")
-  .then(response => response.json())
-  .then(beers => {
-    for (let i = 0; i < beers.length; i++){
-   console.log(beers[i].name)
-    }
-  })
-  .catch(err => console.error(err))
+// fetch("https://api.sampleapis.com/beers/ale")
+//   .then(response => response.json())
+//   .then(beers => {
+//     for (let i = 0; i < beers.length; i++){
+//    console.log(beers[i].name)
+//     }
+//   })
+//   .catch(err => console.error(err))
 
+
+
+  // JO expl   ('url', 'obj header' )
+  function goGetBeers (){
+    const newBeer = { price: '$7', name: 'Stela Artois', id: 220}
+  fetch("https://api.sampleapis.com/beers/ale") // go and get the data
+    .then(myResponse => myResponse.json()) // formating the response to json
+    .then(allBeers => console.log(allBeers[99].price))
+    .then(data => {
+      data.push(newBeer)// manipulating data
+    console.log(data[180])
+  })
+   // .catch((myErr) => {console.log ('We got an error:', myErr)})
+    .catch(myErr => console.error('We got an error:', myErr))
+
+fetch('httpps://api.smaplesapis.com/coffee/hot')
+.then(allCoffees => allCoffees.json())
+.then(formattedCoffes => (coffees = formattedCoffes))
+.catch(err => console.err(err))
+
+console.log(allBeers, allCoffees)
+return [...allBeers , ...allCoffees]
+}
+
+
+
+
+
+
+
+
+goGetBeers ()
+
+// // one way to write anonymous 
+// function (myErr) {
+//   return console.log(myErr);
+//   console.log('test') // this line will never run
+// }
+
+
+// // arrow function way
+// () => {}
+
+// () => {
+//   return console.log('test') // explicit return
+//   console.log('test') // this line will never run
+// }
+
+
+
+// () => {}
